78385148900370afda55f3662df6cf18a5de32e9
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-04-30 18:08:30 -0300

    FEAT: add infos footer

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const date = new Date();
+const year = date.getFullYear();
+
+
+
+document.getElementById("date").innerText = year;
+
 function responsiveNav() {
-    document.getElementsByClassName("nav")[0].classList.toggle("responsive");
+  document.getElementsByClassName("nav")[0].classList.toggle("responsive");
 }
